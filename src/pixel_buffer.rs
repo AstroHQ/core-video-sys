f8c80b1cdb05a971c54089282c3377edc73fbfee
mihirsamdarshi <msamdars@stanford.edu>
2022-01-04 10:57:39 -0800

    style: clippy lints

--- src/pixel_buffer.rs
+++ src/pixel_buffer.rs
@@ -12,7 +12,7 @@ const fn as_u32_be(array: &[u8; 4]) -> u32 {
     ((array[0] as u32) << 24)
         + ((array[1] as u32) << 16)
         + ((array[2] as u32) << 8)
-        + ((array[3] as u32) << 0)
+        + (array[3] as u32)
 }
 
 pub type CVPixelBufferLockFlags = u64;
